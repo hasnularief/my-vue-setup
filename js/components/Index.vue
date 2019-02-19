00d04d5a11f7f66a1441e94fef61f46355d61d64
Hasnul Arief Fikri <arief.hasnul@gmail.com>
2019-02-19 12:20:44 +0700

    Create Index.vue

---
+++ js/components/Index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>INDEX</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      this.f1().then(this.f2().then(this.f3()))
+    },
+
+    methods: {
+      f1() {
+        return new Promise(function(resolve){
+          setTimeout(function(){
+            // console.log("OKE F1")	
+            resolve()
+          }, 1000)	
+        })
+      },
+
+      f2() {
+        return new Promise(function(resolve){
+          setTimeout(function(){
+            // console.log("OKE F2")	
+            resolve()
+          }, 2000)	
+        })
+      },
+
+      f3() {
+        return new Promise(function(resolve){
+          setTimeout(function(){
+            // console.log("OKE F3")	
+            resolve()
+          }, 2000)	
+        })
+      },
+    }
+  }
+</script>
