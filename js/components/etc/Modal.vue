72b52c490bb6973ad8debe7426ef8c52f4f89aa0
Hasnul Arief Fikri <arief.hasnul@gmail.com>
2019-02-19 12:22:13 +0700

    Create Modal.vue

---
+++ js/components/etc/Modal.vue
@@ -0,0 +1,75 @@
+<script>
+  import $ from 'jquery'
+  export default {
+    props: {
+      width:{
+        default: '',
+        type: String
+      }, 
+      loading:{
+        default: false,
+        type: Boolean
+      },
+      bodyStyle:{
+        default: '',
+        type: String
+      },
+      height:{
+        default: '',
+        type: String
+      },
+      disabledButton:{
+        default: false,
+        type: Boolean
+      }
+    },
+    data () {
+      return {
+
+      }
+    },
+
+    created () {        
+
+    },
+    mounted(){
+      $('.select-multi').selectize()
+    }
+  }
+</script>
+<template>
+  <div>
+    <div role="dialog" class="modal fade show d-block">
+      <div class="modal-dialog modal-md">
+        <div tabindex="-1" role="document" class="modal-content">
+          <header class="modal-header">
+            <h5 class="modal-title">
+              <slot name="header">
+                Konfirmasi Hapus
+              </slot>
+            </h5>
+            <button type="button" aria-label="Close" class="close" @click="$emit('close')">
+              ×
+            </button>
+          </header>
+          <div class="modal-body" style="padding-left: 15px; padding-right: 15px;">
+            <slot name="body">
+              Apa anda yakin akan menghapus rekord?
+            </slot>
+          </div>
+          <footer class="modal-footer">
+            <slot name="footer">
+              <button type="button" class="btn btn-sm btn-danger" @click="$emit('close')">
+                Cancel
+              </button>
+              <button type="button" class="btn btn-sm btn-primary" @click="$emit('submit')">
+                OK
+              </button>
+            </slot>
+          </footer>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop fade show" />
+  </div>
+</template>
