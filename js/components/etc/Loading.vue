341fefd3f40ee84338030cdb924c14b3f0900ec2
Hasnul Arief Fikri <arief.hasnul@gmail.com>
2019-02-19 12:21:47 +0700

    Create Loading.vue

---
+++ js/components/etc/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="mode == 'card'" class="overlay">
+    <i class="fa fa-sync-alt fa-spin" />
+  </div>
+  <div v-else-if="mode == 'modal'" class="modal-overlay">
+    <i class="fa fa-sync-alt fa-spin" />
+  </div>
+</template>
+
+<script>
+  export default {
+   props: {
+    mode: {
+      type: String,
+      default: 'card'
+   }
+ }
+}
+</script>
